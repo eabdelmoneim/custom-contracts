bd8333b6a92e279882ce6ac545866efc207508e3
Krishang Nadgauda <nkrishang@Krishangs-MacBook-Pro.local>
2022-07-29 09:53:13 -0400

    Use ERC721LazyMint from Solidity SDK

--- contracts/ThirdwebRentableERC721A.sol
+++ contracts/ThirdwebRentableERC721A.sol
@@ -2,22 +2,16 @@
 
 pragma solidity ^0.8.4;
 
-import "erc721a/contracts/ERC721A.sol";
+import "@thirdweb-dev/contracts/base/ERC721LazyMint.sol";
 import "@thirdweb-dev/contracts/extension/PermissionsEnumerable.sol";
-import "@thirdweb-dev/contracts/extension/LazyMint.sol";
-import "@thirdweb-dev/contracts/extension/ContractMetadata.sol";
-import "@thirdweb-dev/contracts/extension/Royalty.sol";
-import "@thirdweb-dev/contracts/extension/Ownable.sol";
 
-contract ThirdwebRentableERC721A is ERC721A, PermissionsEnumerable,  ContractMetadata, Ownable, Royalty, LazyMint {
+contract ThirdwebRentableERC721A is ERC721LazyMint, PermissionsEnumerable {
 
     // DEFINE YOUR ROLE
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");  
     bytes32 private constant TRANSFER_ROLE = keccak256("TRANSFER_ROLE");
 
     address private deployer;
-
-    mapping(uint256 => string) private fullURI;
 
 
     /*//////////////////////////////////////////////////////////////
@@ -32,8 +26,7 @@ contract ThirdwebRentableERC721A is ERC721A, PermissionsEnumerable,  ContractMet
         string memory _symbol,
         address _royaltyRecipient,
         uint128 _royaltyBps
-    ) ERC721A(_name, _symbol) {
-
+    ) ERC721LazyMint(_name, _symbol, _royaltyRecipient, _royaltyBps) {
         deployer = msg.sender;
 
         // set all permissions
@@ -44,25 +37,13 @@ contract ThirdwebRentableERC721A is ERC721A, PermissionsEnumerable,  ContractMet
 
         // set royalty data
         _setupDefaultRoyaltyInfo(_royaltyRecipient, _royaltyBps);
-
-    }
-
-    /*
-    *   Since both ERC721A and thirdweb Royalty extension implement ERC2981 need supportsInterface
-    */
-    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721A, IERC165) returns (bool) {
-        return
-            interfaceId == 0x01ffc9a7 || // ERC165 Interface ID for ERC165
-            interfaceId == 0x80ac58cd || // ERC165 Interface ID for ERC721
-            interfaceId == 0x5b5e139f || // ERC165 Interface ID for ERC721Metadata
-            interfaceId == type(IERC2981).interfaceId; // ERC165 ID for ERC2981
     }
     
-     /*//////////////////////////////////////////////////////////////////////////
+    /*//////////////////////////////////////////////////////////////////////////
                             THIRDWEB OWNABLE FEATURE OVERRIDES
     //////////////////////////////////////////////////////////////////////////*/
 
-    function _canSetOwner() internal virtual override returns (bool) {
+    function _canSetOwner() internal view virtual override returns (bool) {
         return msg.sender == owner();
     }
 
@@ -73,7 +54,7 @@ contract ThirdwebRentableERC721A is ERC721A, PermissionsEnumerable,  ContractMet
     /*
     ** only admins/owners can update the contract URI
     */
-    function _canSetContractURI() internal virtual override returns (bool){
+    function _canSetContractURI() internal view virtual override returns (bool){
         return msg.sender == deployer || hasRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
@@ -84,7 +65,7 @@ contract ThirdwebRentableERC721A is ERC721A, PermissionsEnumerable,  ContractMet
     /*
     *   Only admins/owners can set royalties
     */
-    function _canSetRoyaltyInfo() internal virtual override returns (bool){
+    function _canSetRoyaltyInfo() internal view virtual override returns (bool){
         return msg.sender == deployer || hasRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
@@ -92,40 +73,13 @@ contract ThirdwebRentableERC721A is ERC721A, PermissionsEnumerable,  ContractMet
                             THIRDWEB LAZY MINT FEATURE OVERRIDES
     //////////////////////////////////////////////////////////////////////*/
 
-    /**
-     *  @notice         Returns the metadata URI for an NFT.
-     *  @dev            See `BatchMintMetadata` for handling of metadata in this contract.
-     *
-     *  @param _tokenId The tokenId of an NFT.
-     */
-    function tokenURI(uint256 _tokenId) public view virtual override returns (string memory) {
-        string memory fullUriForToken = fullURI[_tokenId];
-        if (bytes(fullUriForToken).length > 0) {
-            return fullUriForToken;
-        }
-
-        string memory batchUri = getBaseURI(_tokenId);
-        return string(abi.encodePacked(batchUri, _toString(_tokenId)));
-    }
-
      /**
      *
      *  We make it so that anyone can mint tokens that have already been lazy minted.
      */
-    function mintTo(address _to, string memory _tokenURI) public virtual  {
-
-        // check permissions
+    function mintTo(address _to, string memory _tokenURI) public virtual override  {
         require(hasRole(MINTER_ROLE, msg.sender),"must have minter role to mint");
-
-        // check to make sure there are lazy minted tokens to be minted
-        uint256 quantity = 1;
-        require(
-            _nextTokenId() + quantity <= nextTokenIdToLazyMint,
-            "Not enough lazy minted tokens."
-        );
-
-        fullURI[nextTokenIdToLazyMint] = _tokenURI;
-        _safeMint(_to, quantity, "");
+        super.mintTo(_to, _tokenURI);
     }
 
    /**
